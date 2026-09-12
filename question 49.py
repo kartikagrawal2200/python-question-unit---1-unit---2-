9c0fe786aaafece2fcc71edfdc48c5fbcb29af72
KARTIK AGRAWAL <kartikagrawalsheopur1245@gmail.com>
2026-09-12 16:12:39 +0530

    Implement command input handling for START, STOP, PAUSE

---
+++ question 49.py
@@ -0,0 +1,8 @@
+command = input().upper()
+if command == "START":
+    print("Running")
+elif command == "STOP":
+    print("Stopped")
+elif command == "PAUSE":
+    pass
+    print("Feature Pending")
